f2fda4220de835d84d2a7362368d0cc71f5fe9d7
Luke Cowell <luke@ekrata.com>
2022-05-15 14:13:39 +1000

    added subscribe

--- src/lib/components/api/firebase/_firebase.ts
+++ src/lib/components/api/firebase/_firebase.ts
@@ -3,19 +3,18 @@ import * as firebaseui from 'firebaseui';
 import { initializeApp } from 'firebase/app';
 import 'firebaseui/dist/firebaseui.css';
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_KEY as string,
-  authDomain: 'PROJECT_ID.firebaseapp.com',
-  // The value of `databaseURL` depends on the location of the database
-  databaseURL: 'https://DATABASE_NAME.firebaseio.com',
-  projectId: 'PROJECT_ID',
-  storageBucket: 'PROJECT_ID.appspot.com',
-  messagingSenderId: 'SENDER_ID',
-  appId: 'APP_ID',
-  // For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
-  measurementId: 'G-MEASUREMENT_ID',
+	apiKey: import.meta.env.VITE_FIREBASE_KEY as string,
+	authDomain: 'PROJECT_ID.firebaseapp.com',
+	// The value of `databaseURL` depends on the location of the database
+	databaseURL: 'https://DATABASE_NAME.firebaseio.com',
+	projectId: 'PROJECT_ID',
+	storageBucket: 'PROJECT_ID.appspot.com',
+	messagingSenderId: 'SENDER_ID',
+	appId: 'APP_ID',
+	// For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
+	measurementId: 'G-MEASUREMENT_ID'
 };
 
-const app = initializeApp(firebaseConfig);
-const ui = new firebaseui.auth.AuthUI(getAuth(), app.name);
-
-export default ui;
+export const app = initializeApp(firebaseConfig);
+export const ui = new firebaseui.auth.AuthUI(getAuth(), app.name);
+export const db = getFirestore(app);
---
+++ src/lib/forms/MailSubscribe/MailSubscribe.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+	import type { InferType, ValidationError } from 'yup';
+	import TextField from '$lib/components/form/TextField.svelte';
+	import { Button } from '$lib/components/nav';
+	import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+	import { doc, setDoc } from 'firebase/firestore';
+	import Fa from 'svelte-fa';
+	import { values, schema } from './schema';
+	import { onMount } from 'svelte';
+	let errors = {
+		email: ''
+	};
+
+	onMount(async () => {
+		const { db } = await import('$lib/components/api/firebase/_firebase');
+
+		const handleSubmit = async () => {
+			try {
+				const validatedValues = await schema.validate(values, { abortEarly: false });
+				await setDoc(doc(db, 'EmailList', ''), { email: validatedValues.email });
+				errors = { email: '' };
+			} catch (err: any) {
+				const error = err as ValidationError;
+				errors = error.inner.reduce((acc, err) => {
+					if (error.path) {
+						return { ...acc, [error.path]: error.message };
+					}
+					return { ...acc };
+				}, {});
+			}
+		};
+	});
+</script>
+
+<form method="POST" class="text-normal flex flex-col" on:submit|preventDefault={handleSubmit}>
+	<TextField
+		bind:field={values.email}
+		bind:error={errors.email}
+		type="email"
+		label="Subscribe to receive updates about our esports data plans, including new games, helpful guides, and new ekrata products"
+	>
+		<Fa slot="icon" icon={faEnvelope} primaryColor="gray" secondaryColor="white" />
+	</TextField>
+	<div class="lg:mx-auto lg:w-1/2 mt-10">
+		<Button toggle alternate type="submit">
+			<div slot="text" class="zinc-200 align-center text-normal flex gap-x-4 py-1">
+				<Fa
+					icon={faEnvelope}
+					secondaryOpacity={1}
+					size="lg"
+					primaryColor="white"
+					secondaryColor="white"
+				/>
+				<div class="inline font-sans font-medium">Sign up</div>
+			</div>
+		</Button>
+	</div>
+</form>
